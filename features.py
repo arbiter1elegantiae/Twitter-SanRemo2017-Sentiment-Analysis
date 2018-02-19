37ce37262f8e5b87cdaf11141ef407b681e353b1
Oscar Benedetti <oscarbene@hotmail.it>
2018-02-20 00:58:37 +0100

    updated bigramVectorizer and added list of positive/negative italian words

--- features.py
+++ features.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-df = pd.read_csv('cleanedData.tsv')
+df = pd.read_csv('provaParole.tsv')
 
-gramVectorizer = CountVectorizer(min_df=5)
-bowTransformer = gramVectorizer.fit_transform(df['tweet_text'].values.astype(str))
+bigramVectorizer = CountVectorizer(min_df=5, ngram_range=(1, 2))
+bowTransformer = bigramVectorizer.fit_transform(df['tweet_text'].values.astype(str))
 
+print(bigramVectorizer.get_feature_names())
 
-print(gramVectorizer.get_feature_names())
+print(bowTransformer.toarray())
