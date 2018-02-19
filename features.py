f7d740172e8d9a3732744a1887cda0817f10942b
federico peconi <federico.peconi@studio.unibo.it>
2018-02-19 13:53:34 +0100

    Vectorizing with scikit

--- features.py
+++ features.py
@@ -1,45 +1,10 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-grams = {}
-corpus =[] 
-newDf = pd.DataFrame()
+df = pd.read_csv('cleanedData.tsv')
 
-# def featureVectorBuilder():
-# # select only those 1-gram that occurs more than 30 times in the dataset to bound feature space (also more domain specific)
-#     df = pd.read_table('cleanedData.tsv')
-#     for row in df.itertuples():
-#         for key in row[1].split(',')[1].split(' '):
-#           grams[key] = grams.get(key, 1) + 1       
-          
-
-# featureVectorBuilder()
-# print([key for key in grams if grams[key] > 30])
-
-def dfToList():
-    vectorizer = CountVectorizer()
-    df = pd.read_table('cleanedData.tsv', names = ['id'])
-    for row in df.itertuples():
-        grams = row[1].split(',')
-        print(grams[1])
-        corpus.append(grams[1])
-
-    newDf['text'] = corpus  
-
-    print(corpus)
-    # print('\n\n\n\n\n\n\n\n')
-    # print(newDf)
-
-    X = vectorizer.fit_transform(corpus)
-    print(X)
+gramVectorizer = CountVectorizer(min_df=5)
+bowTransformer = gramVectorizer.fit_transform(df['tweet_text'].values.astype(str))
 
 
-    print(vectorizer.get_feature_names())
-    X.toarray()
-
-    print(X)
-
-if __name__ == "__main__":
-
-    dfToList()
-
+print(gramVectorizer.get_feature_names())
