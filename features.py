61b4de9f61b1cb8f5836da7aaae79caddaa63728
federico peconi <federico.peconi@studio.unibo.it>
2018-02-21 05:40:18 +0100

    Features Vectors apparently builded
    Now we have to train (multivariate Naive Bayes) and see what happens

--- features.py
+++ features.py
@@ -1,11 +1,105 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk import stem
+from nltk.stem.snowball import SnowballStemmer
 
-df = pd.read_csv('provaParole.tsv')
 
-bigramVectorizer = CountVectorizer(min_df=5, ngram_range=(1, 2))
-bowTransformer = bigramVectorizer.fit_transform(df['tweet_text'].values.astype(str))
 
-print(bigramVectorizer.get_feature_names())
 
-print(bowTransformer.toarray())
+def buildNegPolarityFeat(df, dfNeg):
+    
+    negColumn=[]
+    
+    for tweet in df.itertuples(index=False):
+        
+        tweetList = str(tweet[1]).split(' ')
+        del tweetList[-1] #last element is '', doesnt count then remove
+        
+        oldStem = ''
+        negPolarity = 0
+        for row in dfNeg.itertuples(index=False):
+            stem = stemmer.stem(row[0])
+            if oldStem != stem:
+
+                if stem in tweetList:
+                    negPolarity += 1
+                oldStem = stem
+        #round negative polarity of the tweet to the closest multiple of 5
+        print('NEGFeature'+str(tweet[0])+'-->'+str(int(5 * round(float(negPolarity)/5)))) #print work in progress
+        negColumn.append(int(3 * round(float(negPolarity)/3)))
+    
+    
+    return np.array(negColumn)
+
+
+
+
+def buildPosPolarityFeat(df, dfPos):
+    
+    posColumn=[]
+    
+    for tweet in df.itertuples(index=False):
+        
+        tweetList = str(tweet[1]).split(' ')
+        del tweetList[-1] #last element is '', doesnt count then remove
+        
+        oldStem = ''
+        posPolarity = 0
+        for row in dfPos.itertuples(index=False):
+            stem = stemmer.stem(row[0])
+            if oldStem != stem:
+
+                if stem in tweetList:
+                    posPolarity += 1
+                oldStem = stem
+        #round positive polarity of the tweet to the closest multiple of 5
+        print('POSFeature'+str(tweet[0])+'-->'+str(int(3 * round(float(posPolarity)/3)))) #print work in progress
+        posColumn.append(int(3 * round(float(posPolarity)/3)))
+
+    
+    return np.array(posColumn)      
+
+
+def buildEmojiPolarity(df):
+    emoji=[]
+
+    for tweet in df.itertuples(index=False):
+        
+        emojiPolarity=0
+        for emoji in tweet[2]:
+            
+            if emoji == '__EMOT_POS':
+                emojiPolarity+=1
+            elif emoji == '__EMOT_NEG':
+                emojiPolarity-=1
+        
+        print('EMOFeature'+str(tweet[0])+'-->'+str(int(2 * round(float(emojiPolarity)/2)))) #print work in progress        
+        emoji.append(int(2 * round(float(emojiPolarity)/2)))
+    
+    return np.array(emoji)
+
+
+
+
+stemmer = SnowballStemmer("italian")
+
+df = pd.read_csv('../cleanedData.tsv')
+
+dfNeg = pd.read_csv('./lexicon/neg.words.txt',header=None,names=['word'])
+dfPos = pd.read_csv('./lexicon/pos.words.txt',header=None,names=['word'])
+
+gramVectorizer = CountVectorizer(min_df=5, ngram_range=(1, 1))
+bowTransformer = gramVectorizer.fit_transform(df['tweet_text'].values.astype(str))
+
+featuresVecTmp = bowTransformer.toarray()
+
+negPolarityFeat=buildNegPolarityFeat(df, dfNeg)
+posPolarityFeat=buildPosPolarityFeat(df, dfPos)
+emojiPolarityFeat=buildEmojiPolarity(df)
+
+print(negPolarityFeat.shape, posPolarityFeat.shape, emojiPolarityFeat.shape)
+
+featuresVec=np.c_[featuresVecTmp, negPolarityFeat, posPolarityFeat, emojiPolarityFeat]
+
+
+print(featuresVec)
