aed9022dae4a4bacecd8d8e4ac161b152853ae72
Oscar Benedetti <oscarbene@hotmail.it>
2018-02-24 03:23:15 +0100

    aded function to build classification vector

--- features.py
+++ features.py
@@ -80,11 +80,30 @@ def buildEmojiPolarity(df):
     return np.array(emojiColumn)
 
 
+def buildClassificationVector(df, dfClassified):
+    classificationColumn=[]
+
+    for tweet in df.itertuples(index=False):
+
+        for classifiedTweet in dfClassified.itertuples(index=False):
+
+            if (tweet[0] == classifiedTweet[0]):
+                if (classifiedTweet[3] == 'negative'):
+                    classificationColumn.append((classifiedTweet[0], -1))
+                if (classifiedTweet[3] == 'neutral'):
+                    classificationColumn.append((classifiedTweet[0], 0))
+                if (classifiedTweet[3] == 'positive'):
+                    classificationColumn.append((classifiedTweet[0], 1))    
+
+    return (np.array(classificationColumn))
+
+
 
 
 stemmer = SnowballStemmer("italian")
 
-df = pd.read_csv('../cleanedData.tsv')
+df = pd.read_csv('./cleanedData.tsv')
+dfClassified = pd.read_csv('./classified.tsv')
 
 dfNeg = pd.read_csv('./lexicon/neg.words.txt',header=None,names=['word'])
 dfPos = pd.read_csv('./lexicon/pos.words.txt',header=None,names=['word'])
@@ -97,9 +116,11 @@ featuresVecTmp = bowTransformer.toarray()
 negPolarityFeat=buildNegPolarityFeat(df, dfNeg)
 posPolarityFeat=buildPosPolarityFeat(df, dfPos)
 emojiPolarityFeat=buildEmojiPolarity(df)
+classificationFeat = buildClassificationVector(df, dfClassified)
 
 print(negPolarityFeat.shape, posPolarityFeat.shape, emojiPolarityFeat.shape)
-
+print(classificationFeat.shape)
+print(classificationFeat)
 featuresVec=np.c_[featuresVecTmp, negPolarityFeat, posPolarityFeat, emojiPolarityFeat]
 
 
