47a9fe646b53855e5b68b148ea07ef23c9a41a65
Oscar Benedetti <oscarbene@hotmail.it>
2018-02-27 12:59:29 +0100

    filter.py v1.1 - It creates a tsv only with ID - Text - SInger

--- filter.py
+++ filter.py
@@ -15,13 +15,7 @@ from nltk import stem
 from nltk.stem.snowball import SnowballStemmer
 
 
-
-fNames = ['tweet_id_str','tweet_created_at','tweet_text','tweet_source','tweet_hashtags',
-           'tweet_urls','tweet_user_mentions','tweet_media','tweet_in_reply_to_screen_name',
-           'tweet_in_reply_to_status_id','tweet_retweeted','tweet_retweeted_status_user_screen_name',
-           'tweet_retweeted_status_id','user_id_str','user_name','user_screen_name','user_description',
-           'user_url','user_followers_count','user_friends_count','user_created_at','user_statuses_count',
-           'user_profile_image_url','user_location','tweet_lang','tweet_favorite_count','tweet_retweet_count']
+fNames = ['tweet_id_str','tweet_text', 'singers']
 
 fields = fNames
 
@@ -35,36 +29,12 @@ def findParticipantsTweets(df):
         for word in tweet[3].split():
 
             if (word.lower() in nomiPartecipanti):
-                participantsDF = participantsDF.append({'tweet_id_str': tweet.tweet_id_str,
-                                                        'tweet_created_at': tweet.tweet_created_at,
-                                                        'tweet_text': tweet.tweet_text, 
-                                                        'tweet_source': tweet.tweet_source, 
-                                                        'tweet_hashtags': tweet.tweet_hashtags, 
-                                                        'tweet_urls': tweet.tweet_urls,
-                                                        'tweet_user_mentions': tweet.tweet_user_mentions, 
-                                                        'tweet_media': tweet.tweet_media, 
-                                                        'tweet_in_reply_to_screen_name': tweet.tweet_in_reply_to_screen_name, 
-                                                        'tweet_in_reply_to_status_id': tweet.tweet_in_reply_to_status_id,
-                                                        'tweet_retweeted': tweet.tweet_retweeted,
-                                                        'tweet_retweeted_status_user_screen_name': tweet.tweet_retweeted_status_user_screen_name,
-                                                        'tweet_retweeted_status_id': tweet.tweet_retweeted_status_id,
-                                                        'user_id_str': tweet.user_id_str,
-                                                        'user_name': tweet.user_name,
-                                                        'user_screen_name': tweet.user_screen_name,
-                                                        'user_description': tweet.user_description,
-                                                        'user_url': tweet.user_url,
-                                                        'user_followers_count': tweet.user_followers_count,
-                                                        'user_friends_count': tweet.user_friends_count,
-                                                        'user_created_at': tweet.user_created_at,
-                                                        'user_statuses_count': tweet.user_statuses_count,
-                                                        'user_profile_image_url': tweet.user_profile_image_url,
-                                                        'user_location': tweet.user_location,
-                                                        'tweet_lang': tweet.tweet_lang,
-                                                        'tweet_favorite_count': tweet.tweet_favorite_count,
-                                                        'tweet_retweet_count': tweet.tweet_retweet_count}, ignore_index=True)
-        
+                participantsDF = participantsDF.append({'tweet_id_str': tweet.tweet_id_str,'tweet_text': tweet.singers, 'singers': word}, ignore_index=True)
+    
+    participantsDF.to_csv('participants.tsv')    
     print(participantsDF)
 
+    
 
 if __name__ == "__main__":
 
